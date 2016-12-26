5cd38e12a7a8e9fdc6a79050b2e597a02f276275
Diogo Autilio <diogo.autilio@concrete.com.br>
2016-12-26 11:11:39 -0200

    Initial Commit

---
+++ SWDestiny Trades/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 26/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SWDestiny Trades/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 26/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
