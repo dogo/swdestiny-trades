81462ddd9a2639988b92fc9e3360c617b587cac7
Diogo Autilio <diogo.autilio@concrete.com.br>
2016-12-27 10:07:07 -0200

    Create alphabetic sections

--- SWDestiny Trades/Classes/Cards/CardList/Controller/CardListViewController.swift
+++ SWDestiny Trades/Classes/Cards/CardList/Controller/CardListViewController.swift
@@ -12,7 +12,8 @@ import Alamofire
 class CardListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView?
-    var swdCards: [CardDTO] = []
+    var swdCards: [Character : [CardDTO]] = [ : ]
+    var sectionLetters: [Character] = []
 
     // MARK: - Life Cycle
 
@@ -20,9 +21,7 @@ class CardListViewController: UIViewController, UITableViewDelegate, UITableView
         super.viewDidLoad()
 
         CardsAPIClient.retrieveCardList(successBlock: { (cardsArray: Array<CardDTO>) in
-            self.swdCards = cardsArray.sorted {
-                $0.name < $1.name
-            }
+            self.getTableData(cardList: cardsArray)
             self.tableView?.reloadData()
         }) { (error: DataResponse<Any>) in
             print(error)
@@ -32,8 +31,8 @@ class CardListViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        if let path = self.tableView?.indexPathForSelectedRow {
-            self.tableView?.deselectRow(at: path, animated: animated)
+        if let path = tableView?.indexPathForSelectedRow {
+            tableView?.deselectRow(at: path, animated: animated)
         }
     }
 
@@ -50,15 +49,61 @@ class CardListViewController: UIViewController, UITableViewDelegate, UITableView
             //The impossible happened
             fatalError("Wrong Cell Type")
         }
-        cell.configureCell(cardDTO: self.swdCards[indexPath.row])
+        cell.configureCell(cardDTO: (swdCards[sectionLetters[indexPath.section]]?[indexPath.row])!)
         return cell
     }
 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(sectionLetters[section])
+    }
+
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sectionLetters.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return swdCards.count
+        return swdCards[sectionLetters[section]]!.count
+    }
+
+    // MARK: - Split and Sort UITableView source
+
+    func createTableData(cardList: [CardDTO]) -> (firstLetters: [Character], source: [Character : [CardDTO]]) {
+
+        // Build Character Set
+        var letters = Set<Character>()
+
+        func getFirstLetter(cardDTO: CardDTO) -> Character {
+            return cardDTO.name[cardDTO.name.startIndex]
+        }
+
+        cardList.forEach {_ = letters.insert(getFirstLetter(cardDTO: $0)) }
+
+        // Build tableSource array
+        var tableViewSource = [Character: [CardDTO]]()
+
+        for symbol in letters {
+
+            var cardsDTO = [CardDTO]()
+
+            for card in cardList {
+                if symbol == getFirstLetter(cardDTO: card) {
+                    cardsDTO.append(card)
+                }
+            }
+            tableViewSource[symbol] = cardsDTO.sorted {
+                $0.name < $1.name
+            }
+        }
+
+        let sortedSymbols = letters.sorted {
+            $0 < $1
+        }
+
+        return (sortedSymbols, tableViewSource)
+    }
+
+    func getTableData(cardList: [CardDTO]) {
+        swdCards = createTableData(cardList: cardList).source
+        sectionLetters = createTableData(cardList: cardList).firstLetters
     }
 }
--- SWDestiny Trades/Classes/Cards/SetsList/Controller/SetsListViewController.swift
+++ SWDestiny Trades/Classes/Cards/SetsList/Controller/SetsListViewController.swift
@@ -12,7 +12,8 @@ import Alamofire
 class SetsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView?
-    var swdSets: [SetDTO] = []
+    var sectionLetters: [Character] = []
+    var swdSets: [Character : [SetDTO]] = [ : ]
 
     // MARK: - Life Cycle
 
@@ -20,7 +21,7 @@ class SetsListViewController: UIViewController, UITableViewDelegate, UITableView
         super.viewDidLoad()
 
         SetsAPIClient.retrieveSetList(successBlock: { (setsArray: Array<SetDTO>) in
-            self.swdSets = setsArray
+            self.getTableData(setList: setsArray)
             self.tableView?.reloadData()
         }) { (error: DataResponse<Any>) in
           print(error)
@@ -48,15 +49,61 @@ class SetsListViewController: UIViewController, UITableViewDelegate, UITableView
             //The impossible happened
             fatalError("Wrong Cell Type")
         }
-        cell.configureCell(setDTO: self.swdSets[indexPath.row])
+        cell.configureCell(setDTO: (swdSets[sectionLetters[indexPath.section]]?[indexPath.row])!)
         return cell
     }
 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(sectionLetters[section])
+    }
+
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sectionLetters.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return swdSets.count
+        return swdSets[sectionLetters[section]]!.count
+    }
+
+    // MARK: - Split and Sort UITableView source  
+
+    func createTableData(setList: [SetDTO]) -> (firstLetters: [Character], source: [Character : [SetDTO]]) {
+
+        // Build Character Set
+        var letters = Set<Character>()
+
+        func getFirstLetter(setDTO: SetDTO) -> Character {
+            return setDTO.name[setDTO.name.startIndex]
+        }
+
+        setList.forEach {_ = letters.insert(getFirstLetter(setDTO: $0)) }
+
+        // Build tableSource array
+        var tableViewSource = [Character: [SetDTO]]()
+
+        for symbol in letters {
+
+            var setsDTO = [SetDTO]()
+
+            for set in setList {
+                if symbol == getFirstLetter(setDTO: set) {
+                    setsDTO.append(set)
+                }
+            }
+            tableViewSource[symbol] = setsDTO.sorted {
+                $0.name < $1.name
+            }
+        }
+
+        let sortedSymbols = letters.sorted {
+            $0 < $1
+        }
+
+        return (sortedSymbols, tableViewSource)
+    }
+
+    func getTableData(setList: [SetDTO]) {
+        swdSets = createTableData(setList: setList).source
+        sectionLetters = createTableData(setList: setList).firstLetters
     }
 }
