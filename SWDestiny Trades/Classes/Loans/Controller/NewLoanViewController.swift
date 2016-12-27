0fd82a04594fc08564626b18c139b8953b6a9470
Diogo Autilio <diogo.autilio@concrete.com.br>
2016-12-27 10:53:33 -0200

    Add dummy NewLoanViewController class

---
+++ SWDestiny Trades/Classes/Loans/Controller/NewLoanViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewLoanViewController.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 27/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import Foundation
