84ecf7163e489b41e4d088e402332757b9821315
Diogo Autilio <diogo.autilio@concrete.com.br>
2016-12-27 11:21:40 -0200

    People Edit mode

--- SWDestiny Trades/Classes/Loans/Controller/LoansViewController.swift
+++ SWDestiny Trades/Classes/Loans/Controller/LoansViewController.swift
@@ -8,16 +8,62 @@
 
 import UIKit
 
-class LoansViewController: UIViewController {
+class LoansViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView?
+    var names: [String] = ["Panda lele lalala"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: IBActions
+
+    @IBAction func editButtonTouched(_ sender: Any) {
+        if self.isEditing {
+            super.setEditing(false, animated: true)
+            tableView?.setEditing(false, animated: true)
+            navigationItem.leftBarButtonItem?.title = "Edit"
+            navigationItem.leftBarButtonItem?.style = .plain
+        } else {
+            super.setEditing(true, animated: true)
+            tableView?.setEditing(true, animated: true)
+            navigationItem.leftBarButtonItem?.title = "Done"
+            navigationItem.leftBarButtonItem?.style = .done
+        }
     }
 
+    // MARK: - <UITableViewDataSource>
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LoansCell", for: indexPath) as? UITableViewCell else {
+            //The impossible happened
+            fatalError("Wrong Cell Type")
+        }
+        //cell.configureCell(setDTO: (swdSets[sectionLetters[indexPath.section]]?[indexPath.row])!)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            names.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        if tableView.isEditing {
+            return UITableViewCellEditingStyle.delete
+        }
+        return UITableViewCellEditingStyle.none
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
 }
