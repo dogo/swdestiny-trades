2974de8904511065c00bc31d02555b2c64e3442b
Diogo Autilio <diogo.autilio@concrete.com.br>
2016-12-28 10:39:08 -0200

    Add search function

---
+++ SWDestiny Trades/Classes/Search/Controller/SearchListViewController.swift
@@ -0,0 +1,84 @@
+//
+//  SearchListViewController.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 28/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class SearchListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+
+    @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var searchBar: UISearchBar!
+    var cardsData: [CardDTO] = []
+    var filtered: [CardDTO] = []
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        CardsAPIClient.retrieveCardList(successBlock: { (cardsArray: Array<CardDTO>) in
+            self.cardsData = cardsArray
+        }) { (error: DataResponse<Any>) in
+            print(error)
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if let path = tableView?.indexPathForSelectedRow {
+            tableView?.deselectRow(at: path, animated: animated)
+        }
+    }
+
+    // MARK: - <UITableViewDelegate>
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "CardDetailsSegue", sender: filtered[indexPath.row])
+    }
+
+    // MARK: - <UITableViewDataSource>
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CardSearchCell.cellIdentifier(), for: indexPath) as? CardSearchCell else {
+            //The impossible happened
+            fatalError("Wrong Cell Type")
+        }
+        cell.configureCell(cardDTO: filtered[indexPath.row])
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filtered.count
+    }
+
+    // MARK: - <UISearchBarDelegate>
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+
+        filtered = cardsData.filter({ (card) -> Bool in
+            let tmp: String = card.name
+            return tmp.range(of: searchText, options: String.CompareOptions.caseInsensitive) != nil
+        })
+        tableView?.reloadData()
+    }
+
+    // MARK: - Segue
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CardDetailsSegue" {
+            if let nextViewController = segue.destination as? CardDetailViewController {
+                nextViewController.cardDTO = sender as? CardDTO
+            }
+        }
+    }
+}
---
+++ SWDestiny Trades/Classes/Search/View/CardSearchCell.swift
@@ -0,0 +1,32 @@
+//
+//  CardSearchCell.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 28/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+
+class CardSearchCell: UITableViewCell {
+
+    @IBOutlet weak var cardNameLabel: UILabel!
+    @IBOutlet weak var cardAffiliationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    internal static func cellIdentifier() -> String {
+        return "CardSearchCell"
+    }
+
+    internal func configureCell(cardDTO: CardDTO) {
+        cardNameLabel.text = cardDTO.name
+        cardAffiliationLabel.text = cardDTO.affiliationName
+        priceLabel.text = ""//cardDTO.price
+    }
+
+    override func prepareForReuse() {
+        cardNameLabel.text = nil
+        cardAffiliationLabel.text = nil
+        priceLabel.text = nil
+    }
+}
