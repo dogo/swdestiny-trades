4dd84c09ec3b308a1885159b8e8b59368f970bba
Diogo Autilio <diogo.autilio@concrete.com.br>
2017-02-14 17:55:00 -0200

    Add About screen test

---
+++ SWDestiny TradesTests/Controllers/AboutViewControllerSpec.swift
@@ -0,0 +1,46 @@
+//
+//  AboutViewControllerSpec.swift
+//  SWDestiny TradesTests
+//
+//  Created by Diogo Autilio on 26/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SWDestiny_Trades
+
+class AboutViewControllerSpec: QuickSpec {
+
+    override func spec() {
+        describe("a about view controller") {
+
+            var controller: AboutViewController!
+            beforeEach {
+                controller = AboutViewController()
+            }
+
+            it("should be able to create a controller") {
+                expect(controller).toNot(beNil())
+            }
+
+            it("should have a view of type") {
+                expect(controller.view).to(beAKindOf(AboutView.self))
+            }
+
+            it("should have the expected navigation title") {
+                let _ = UINavigationController(rootViewController: controller)
+                controller.viewWillAppear(true)
+                expect(controller.navigationItem.title).to(equal(NSLocalizedString("ABOUT", comment: "")))
+            }
+
+#if arch(x86_64) && os(iOS)
+            it("should trigger fatal error if init with coder") {
+                expect { () -> Void in
+                    let _ = AboutViewController(coder: NSCoder())
+                }.to(throwAssertion())
+            }
+#endif
+        }
+    }
+}
