3850d0d1214502f27db00516e2f35c22a95f5b0f
Diogo Autilio <diogo.autilio@concrete.com.br>
2017-03-15 17:18:59 -0300

    Remove facebook from share

--- SWDestiny Trades/Classes/DeckBuilder/Controller/DeckBuilderViewController.swift
+++ SWDestiny Trades/Classes/DeckBuilder/Controller/DeckBuilderViewController.swift
@@ -97,7 +97,7 @@ final class DeckBuilderViewController: UIViewController {
         }
 
         let activityVC = UIActivityViewController(activityItems: [SwdShareProvider(subject: deckDTO.name, text: deckList), "Shared with SWD Trades for iOS"], applicationActivities: nil)
-            activityVC.excludedActivityTypes = [.saveToCameraRoll, .postToFlickr, .postToVimeo, .assignToContact, .addToReadingList]
+            activityVC.excludedActivityTypes = [.saveToCameraRoll, .postToFlickr, .postToVimeo, .assignToContact, .addToReadingList, .postToFacebook]
 
         activityVC.popoverPresentationController?.barButtonItem = sender
         DispatchQueue.global(qos: .userInteractive).async {
