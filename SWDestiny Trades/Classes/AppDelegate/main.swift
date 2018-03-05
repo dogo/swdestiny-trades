80d525a7b2945b8f679a775d092a5e7d8a501d22
Diogo Autilio <diautilio@gmail.com>
2018-03-05 20:56:10 -0300

    Remove main class synthesization

---
+++ SWDestiny Trades/Classes/AppDelegate/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  SWDestiny Trades
+//
+//  Created by Diogo Autilio on 26/12/16.
+//  Copyright © 2016 Diogo Autilio. All rights reserved.
+//
+
+import UIKit
+
+let isRunningTests = NSClassFromString("XCTestCase") != nil
+let appDelegateClass = isRunningTests ? nil : NSStringFromClass(AppDelegate.self)
+let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
+UIApplicationMain(CommandLine.argc, args, nil, appDelegateClass)
