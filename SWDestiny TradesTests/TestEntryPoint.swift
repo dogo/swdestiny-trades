48b3c0ac6aeb391182de27a9b1fa2a349bbd6edd
Diogo Autilio <diautilio@gmail.com>
2020-10-04 12:28:25 -0300

    Add TestEntryPoint class

---
+++ SWDestiny TradesTests/TestEntryPoint.swift
@@ -0,0 +1,16 @@
+//
+//  TestEntryPoint.swift
+//  SWDestiny-TradesTests
+//
+//  Created by Diogo Autilio on 26/12/16.
+//  Copyright © 2017 Diogo Autilio. All rights reserved.
+//
+
+import Foundation
+
+final class TestEntryPoint: NSObject {
+
+    override init() {
+        super.init()
+    }
+}
