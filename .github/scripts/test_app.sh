e020824cb35dd893b465c158829b770fdcca64e1
Diogo Autilio <diautilio@gmail.com>
2022-03-06 12:50:03 -0300

    Setup CI with Tuist

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace swdestiny-trades.xcworkspace \
-            -scheme SWDestiny-Trades \
+xcodebuild -workspace swdestiny-trades-tuist.xcworkspace \
+            -scheme SWDestinyTrades \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean test | xcpretty
