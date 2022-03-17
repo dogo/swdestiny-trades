456c09ff9001d53cc58c0b39bba1ce21d7a47ffe
Diogo Autilio <diautilio@gmail.com>
2022-03-17 13:40:47 -0300

    Fix unit test script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace swdestiny-trades-tuist.xcworkspace \
+xcodebuild -workspace swdestiny-trades.xcworkspace \
             -scheme SWDestinyTrades \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean test | xcpretty
