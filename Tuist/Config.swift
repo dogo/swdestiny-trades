ef4c64e0867aa82f54e0f23fe375c98459eaa61a
Diogo Autilio <diautilio@gmail.com>
2022-05-31 09:11:48 -0300

    Update Tuist to 3.5.9 (was 3.4.0) Add Tuist Cloud config

---
+++ Tuist/Config.swift
@@ -0,0 +1,6 @@
+import ProjectDescription
+
+let config = Config(cloud: .cloud(projectId: "AnyKey/swdestiny-trades",
+                                  url: "https://cloud.tuist.io",
+                                  options: [.optional])
+)
