3185b189764aa462eefd19cf6b164c069a39bf6a
Diogo Autilio <diautilio@gmail.com>
2022-11-05 23:17:47 -0300

    Add LoanDetailNavigatorTests

---
+++ SWDestinyTradesTests/Screens/LoanDetail/Navigator/LoanDetailNavigatorTests.swift
@@ -0,0 +1,42 @@
+//
+//  LoanDetailNavigatorTests.swift
+//  SWDestinyTradesTests
+//
+//  Created by Diogo Autilio on 05/11/22.
+//  Copyright © 2022 Diogo Autilio. All rights reserved.
+//
+
+import Nimble
+import Quick
+import UIKit
+
+@testable import SWDestinyTrades
+
+final class LoanDetailNavigatorTests: QuickSpec {
+
+    override func spec() {
+
+        describe("Loan Detail Navigator") {
+
+            var sut: LoanDetailNavigator!
+            var navigationController: UINavigationControllerMock!
+
+            beforeEach {
+                navigationController = UINavigationControllerMock()
+                sut = LoanDetailNavigator(navigationController)
+            }
+
+            it("should navigate to CardDetailViewController") {
+                sut.navigate(to: .cardDetail(database: nil, with: [.stub()], card: .stub()))
+
+                expect(navigationController.currentPushedViewController).to(beAKindOf(CardDetailViewController.self))
+            }
+
+            it("should navigate to DeckBuilderViewController") {
+                sut.navigate(to: .addCard(database: nil, with: .stub(), type: .borrow))
+
+                expect(navigationController.currentPushedViewController).to(beAKindOf(AddCardViewController.self))
+            }
+        }
+    }
+}
