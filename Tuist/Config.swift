4e5e0b6e5792275b93b5d6549caabef3847d45aa
Diogo Autilio <diautilio@gmail.com>
2022-12-22 23:24:12 -0300

    Send analytics to TuistCloud

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,5 +2,5 @@ import ProjectDescription
 
 let config = Config(cloud: .cloud(projectId: "AnyKey/swdestiny-trades",
                                   url: "https://cloud.tuist.io",
-                                  options: [.optional])
+                                  options: [.optional, .analytics])
 )
