ff8a3a4790f00e901f4883c06b64e81e4f98a47c
Diogo Autilio <diautilio@gmail.com>
2023-08-27 10:01:18 -0300

    Update CI

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild -workspace swdestiny-trades.xcworkspace \
             -scheme SWDestinyTrades \
-            -destination platform=iOS\ Simulator,OS=16.0,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 11 \
             -enableCodeCoverage YES \
             clean test | xcpretty
