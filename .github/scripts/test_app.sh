1fa4d344e00a630b4f09110a1fa10f2620a9b5aa
Diogo Autilio <diautilio@gmail.com>
2023-08-27 11:02:36 -0300

    Update unit test device

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild -workspace swdestiny-trades.xcworkspace \
             -scheme SWDestinyTrades \
-            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=16.4,name=iPhone\ 14 \
             -enableCodeCoverage YES \
             clean test | xcpretty
