48d2ca195002b5bcaa65f05003dca68cf12ebb8a
Diogo Autilio <diautilio@gmail.com>
2023-11-13 09:05:38 -0300

    Migrate testting script to github actions yml

--- .github/scripts/test_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace swdestiny-trades.xcworkspace \
-            -scheme SWDestinyTrades \
-            -destination platform=iOS\ Simulator,OS=17.0,name=iPhone\ 15 Pro \
-            -enableCodeCoverage YES \
-            clean test | xcpretty
