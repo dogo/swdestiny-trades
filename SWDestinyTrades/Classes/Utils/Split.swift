519ab4342450e4006d8bd2f1ae774e618ea7a2e7
Diogo Autilio <diautilio@gmail.com>
2024-01-30 08:26:01 -0300

    Improve Slit functions

--- SWDestinyTrades/Classes/Utils/Split.swift
+++ SWDestinyTrades/Classes/Utils/Split.swift
@@ -10,87 +10,72 @@ import Foundation
 
 enum Split {
     static func cardsAlphabetically(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
+        // Helper function to get the first letter of a card
         func getFirstLetter(cardDTO: CardDTO) -> String {
             return String(cardDTO.name[cardDTO.name.startIndex])
         }
 
-        // Build tableSource array
         var tableViewSource = [String: [CardDTO]]()
 
         for symbol in sections {
-            var cardsDTO = [CardDTO]()
+            let cardsDTO = cardList.filter { getFirstLetter(cardDTO: $0) == symbol }
+                .sorted { $0.name < $1.name }
 
-            for card in cardList where symbol == getFirstLetter(cardDTO: card) {
-                cardsDTO.append(card)
-            }
-            tableViewSource[symbol] = cardsDTO.sorted {
-                $0.name < $1.name
-            }
+            tableViewSource[symbol] = cardsDTO
         }
 
         return tableViewSource
     }
 
     static func cardsByColor(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
+        // Helper function to get the color of a card
         func getColor(cardDTO: CardDTO) -> String {
             return cardDTO.factionCode
         }
 
-        // Build tableSource array
         var tableViewSource = [String: [CardDTO]]()
 
         for symbol in sections {
-            var cardsDTO = [CardDTO]()
+            let cardsDTO = cardList.filter { getColor(cardDTO: $0) == symbol }
+                .sorted { $0.name < $1.name }
 
-            for card in cardList where symbol == getColor(cardDTO: card) {
-                cardsDTO.append(card)
-            }
-            tableViewSource[symbol] = cardsDTO.sorted {
-                $0.name < $1.name
-            }
+            tableViewSource[symbol] = cardsDTO
         }
+
         return tableViewSource
     }
 
     static func cardsByType(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
+        // Helper function to get the type of a card
         func getType(cardDTO: CardDTO) -> String {
             return cardDTO.typeName
         }
 
-        // Build tableSource array
         var tableViewSource = [String: [CardDTO]]()
 
         for symbol in sections {
-            var cardsDTO = [CardDTO]()
+            let cardsDTO = cardList.filter { getType(cardDTO: $0) == symbol }
+                .sorted { $0.name < $1.name }
 
-            for card in cardList where symbol == getType(cardDTO: card) {
-                cardsDTO.append(card)
-            }
-            tableViewSource[symbol] = cardsDTO.sorted {
-                $0.name < $1.name
-            }
+            tableViewSource[symbol] = cardsDTO
         }
 
         return tableViewSource
     }
 
     static func setsByAlphabetically(setList: [SetDTO], sections: [String]) -> [String: [SetDTO]] {
+        // Helper function to get the first letter of a set
         func getFirstLetter(setDTO: SetDTO) -> String {
             return String(setDTO.name.prefix(1))
         }
 
-        // Build tableSource array
         var tableViewSource = [String: [SetDTO]]()
 
         for symbol in sections {
-            var setsDTO = [SetDTO]()
+            let setsDTO = setList.filter { getFirstLetter(setDTO: $0) == symbol }
+                .sorted { $0.name < $1.name }
 
-            for set in setList where symbol == getFirstLetter(setDTO: set) {
-                setsDTO.append(set)
-            }
-            tableViewSource[symbol] = setsDTO.sorted {
-                $0.name < $1.name
-            }
+            tableViewSource[symbol] = setsDTO
         }
 
         return tableViewSource
