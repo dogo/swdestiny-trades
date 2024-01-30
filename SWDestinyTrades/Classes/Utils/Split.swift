72489467dd6a955447140a15884d9aed4fa251b9
Diogo Autilio <diautilio@gmail.com>
2024-01-30 08:34:23 -0300

    Improve even more Split functions

--- SWDestinyTrades/Classes/Utils/Split.swift
+++ SWDestinyTrades/Classes/Utils/Split.swift
@@ -9,75 +9,55 @@
 import Foundation
 
 enum Split {
+
+    static func groupItems<T>(itemList: [T],
+                              sections: [String],
+                              sectionIdentifier: (T) -> String,
+                              sortFunction: (T, T) -> Bool) -> [String: [T]] {
+        var groupedItems = [String: [T]]()
+
+        for symbol in sections {
+            let filteredItems = itemList
+                .filter { sectionIdentifier($0) == symbol }
+                .sorted(by: sortFunction)
+
+            groupedItems[symbol] = filteredItems
+        }
+
+        return groupedItems
+    }
+
     static func cardsAlphabetically(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
-        // Helper function to get the first letter of a card
-        func getFirstLetter(cardDTO: CardDTO) -> String {
+        let sectionIdentifier: (CardDTO) -> String = { cardDTO in
             return String(cardDTO.name[cardDTO.name.startIndex])
         }
 
-        var tableViewSource = [String: [CardDTO]]()
+        let sortFunction: (CardDTO, CardDTO) -> Bool = { $0.name < $1.name }
 
-        for symbol in sections {
-            let cardsDTO = cardList.filter { getFirstLetter(cardDTO: $0) == symbol }
-                .sorted { $0.name < $1.name }
-
-            tableViewSource[symbol] = cardsDTO
-        }
-
-        return tableViewSource
+        return groupItems(itemList: cardList, sections: sections, sectionIdentifier: sectionIdentifier, sortFunction: sortFunction)
     }
 
     static func cardsByColor(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
-        // Helper function to get the color of a card
-        func getColor(cardDTO: CardDTO) -> String {
-            return cardDTO.factionCode
-        }
+        let sectionIdentifier: (CardDTO) -> String = { $0.factionCode }
+        let sortFunction: (CardDTO, CardDTO) -> Bool = { $0.name < $1.name }
 
-        var tableViewSource = [String: [CardDTO]]()
-
-        for symbol in sections {
-            let cardsDTO = cardList.filter { getColor(cardDTO: $0) == symbol }
-                .sorted { $0.name < $1.name }
-
-            tableViewSource[symbol] = cardsDTO
-        }
-
-        return tableViewSource
+        return groupItems(itemList: cardList, sections: sections, sectionIdentifier: sectionIdentifier, sortFunction: sortFunction)
     }
 
     static func cardsByType(cardList: [CardDTO], sections: [String]) -> [String: [CardDTO]] {
-        // Helper function to get the type of a card
-        func getType(cardDTO: CardDTO) -> String {
-            return cardDTO.typeName
-        }
+        let sectionIdentifier: (CardDTO) -> String = { $0.typeName }
+        let sortFunction: (CardDTO, CardDTO) -> Bool = { $0.name < $1.name }
 
-        var tableViewSource = [String: [CardDTO]]()
-
-        for symbol in sections {
-            let cardsDTO = cardList.filter { getType(cardDTO: $0) == symbol }
-                .sorted { $0.name < $1.name }
-
-            tableViewSource[symbol] = cardsDTO
-        }
-
-        return tableViewSource
+        return groupItems(itemList: cardList, sections: sections, sectionIdentifier: sectionIdentifier, sortFunction: sortFunction)
     }
 
     static func setsByAlphabetically(setList: [SetDTO], sections: [String]) -> [String: [SetDTO]] {
-        // Helper function to get the first letter of a set
-        func getFirstLetter(setDTO: SetDTO) -> String {
+        let sectionIdentifier: (SetDTO) -> String = { setDTO in
             return String(setDTO.name.prefix(1))
         }
 
-        var tableViewSource = [String: [SetDTO]]()
+        let sortFunction: (SetDTO, SetDTO) -> Bool = { $0.name < $1.name }
 
-        for symbol in sections {
-            let setsDTO = setList.filter { getFirstLetter(setDTO: $0) == symbol }
-                .sorted { $0.name < $1.name }
-
-            tableViewSource[symbol] = setsDTO
-        }
-
-        return tableViewSource
+        return groupItems(itemList: setList, sections: sections, sectionIdentifier: sectionIdentifier, sortFunction: sortFunction)
     }
 }
