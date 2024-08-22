22cb7b8ff473c6a8f240e98b43137e3617e8f1b1
Diogo Autilio <diautilio@gmail.com>
2024-08-22 08:42:17 -0300

    Update Firebase to 11.1.0 (was 10.20.0)

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/dogo/PKHUD", branch: "fix/spm-resource"),
         .package(url: "https://github.com/liufengting/FTPopOverMenu_Swift", from: "0.4.5"),
         .package(url: "https://github.com/zvonicek/ImageSlideshow", from: "1.9.1"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.20.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "11.1.0"),
         .package(url: "https://github.com/SwiftKickMobile/SwiftMessages", from: "10.0.0-beta"),
         .package(url: "https://github.com/uber/ios-snapshot-test-case", from: "8.0.0"),
         .package(url: "https://github.com/realm/realm-swift", exact: "10.46.0")
