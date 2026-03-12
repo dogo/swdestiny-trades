0a70c511267edc64bd41439f279f6050c68537f0
Diogo Autilio <diautilio@gmail.com>
2026-03-12 07:46:37 -0300

    fix: update firebase-ios-sdk dependency version to 12.10.0

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/danielgindi/Charts", from: "5.1.0"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "5.15.8"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.7.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.10.0"),
         .package(url: "https://github.com/uber/ios-snapshot-test-case", from: "8.0.0"),
         .package(url: "https://github.com/realm/realm-swift", from: "20.0.3")
     ]
