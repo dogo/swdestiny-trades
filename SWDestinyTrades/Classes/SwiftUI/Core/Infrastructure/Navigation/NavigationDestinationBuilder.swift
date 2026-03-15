b9dc06e14f3007d8cfbd07e7418e28f0d4555a78
Diogo Autilio <diautilio@gmail.com>
2026-03-15 09:40:41 -0300

    refactor: enhance navigation destination handling and remove unused wrappers

--- SWDestinyTrades/Classes/SwiftUI/Core/Infrastructure/Navigation/NavigationDestinationBuilder.swift
+++ SWDestinyTrades/Classes/SwiftUI/Core/Infrastructure/Navigation/NavigationDestinationBuilder.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import UIKit
 
 enum AddCardType: Hashable {
     case lent
@@ -20,151 +19,90 @@ enum NavigationDestinationBuilder {
     @ViewBuilder
     static func build(destination: AppDestination) -> some View {
         switch destination {
+        case .setsList, .cardList, .cardDetail, .search:
+            buildCardDestination(destination)
+        case .deckList, .deckBuilder, .deckGraph, .addToDeck:
+            buildDeckDestination(destination)
+        case .peopleList, .newPerson, .loanDetail:
+            buildPeopleDestination(destination)
+        case .userCollection, .addCard, .addCardToCollection, .addCardToPerson:
+            buildCollectionDestination(destination)
+        case .about, .webview:
+            buildMiscDestination(destination)
+        }
+    }
+
+    @ViewBuilder
+    private static func buildCardDestination(_ destination: AppDestination) -> some View {
+        switch destination {
         case .setsList:
             SetsListView()
-
         case let .cardList(setDTO):
             CardListView(set: setDTO)
-
         case let .cardDetail(cards, selectedCard, showAddToCollection):
             CardDetailView(cards: cards, selectedCard: selectedCard, showAddToCollection: showAddToCollection)
-
         case .search:
             SearchView()
+        default:
+            EmptyView()
+        }
+    }
 
+    @ViewBuilder
+    private static func buildDeckDestination(_ destination: AppDestination) -> some View {
+        switch destination {
         case .deckList:
             DeckListView()
-
         case let .deckBuilder(deckDTO):
             DeckBuilderView(deck: deckDTO)
-
         case let .deckGraph(deckDTO):
             DeckGraphView(deck: deckDTO)
-
         case let .addToDeck(deckDTO):
-            AddToDeckViewWrapper(deckDTO: deckDTO)
+            AddToDeckView(deck: deckDTO)
+        default:
+            EmptyView()
+        }
+    }
 
+    @ViewBuilder
+    private static func buildPeopleDestination(_ destination: AppDestination) -> some View {
+        switch destination {
         case .peopleList:
             PeopleListView()
-
         case .newPerson:
-            NewPersonViewWrapper()
-
+            NewPersonView()
         case let .loanDetail(personId):
             LoanDetailView(personId: personId)
+        default:
+            EmptyView()
+        }
+    }
 
+    @ViewBuilder
+    private static func buildCollectionDestination(_ destination: AppDestination) -> some View {
+        switch destination {
         case .userCollection:
             UserCollectionView()
-
         case .addCard:
-            let userCollection = getUserCollection()
-            AddCardView(context: .collection(userCollection))
-
+            AddCardView(context: .collection(UserCollectionDTO()))
         case let .addCardToCollection(userCollection):
             AddCardView(context: .collection(userCollection))
-
         case let .addCardToPerson(personId, type):
             AddCardView(personId: personId, type: type)
+        default:
+            EmptyView()
+        }
+    }
 
+    @ViewBuilder
+    private static func buildMiscDestination(_ destination: AppDestination) -> some View {
+        switch destination {
         case .about:
             AboutView()
-
         case let .webview(url):
             WebViewWrapper(url: url)
+        default:
+            EmptyView()
         }
-    }
-
-    private static func getUserCollection() -> UserCollectionDTO {
-        return UserCollectionDTO()
-    }
-
-    private static func contextForType(person: PersonDTO, type: AddCardType) -> AddCardContext {
-        switch type {
-        case .lent:
-            return .lentToPerson(person)
-        case .borrow:
-            return .borrowedFromPerson(person)
-        case .collection:
-            return .collection(UserCollectionDTO())
-        }
-    }
-}
-
-// MARK: - View Wrappers for UIKit Integration
-
-struct CardListViewWrapper: View {
-    let setDTO: SetDTO
-
-    var body: some View {
-        CardListView(set: setDTO)
-    }
-}
-
-struct CardDetailViewWrapper: View {
-    let cardDTO: CardDTO
-
-    var body: some View {
-        CardDetailView(cards: [cardDTO], selectedCard: cardDTO)
-    }
-}
-
-struct SearchViewWrapper: View {
-    var body: some View {
-        SearchView()
-    }
-}
-
-struct DeckListViewWrapper: View {
-    var body: some View {
-        DeckListView()
-    }
-}
-
-struct DeckBuilderViewWrapper: View {
-    let deckDTO: DeckDTO?
-
-    var body: some View {
-        DeckBuilderView(deck: deckDTO)
-    }
-}
-
-struct DeckGraphViewWrapper: View {
-    let deckDTO: DeckDTO
-
-    var body: some View {
-        DeckGraphView(deck: deckDTO)
-    }
-}
-
-struct AddToDeckViewWrapper: View {
-    let deckDTO: DeckDTO
-
-    var body: some View {
-        AddToDeckView(deck: deckDTO)
-    }
-}
-
-struct NewPersonViewWrapper: View {
-    var body: some View {
-        NewPersonView()
-    }
-}
-
-struct UserCollectionViewWrapper: View {
-    var body: some View {
-        UserCollectionView()
-    }
-}
-
-struct AddCardViewWrapper: View {
-    let setDTO: SetDTO?
-
-    var body: some View {
-        let userCollection = getUserCollection()
-        AddCardView(context: .collection(userCollection))
-    }
-
-    private func getUserCollection() -> UserCollectionDTO {
-        return UserCollectionDTO()
     }
 }
