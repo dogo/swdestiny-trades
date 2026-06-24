35c10038d0cbf5d4bba703e5af1d72eb8ef65dab
Diogo Autilio <diautilio@gmail.com>
2026-06-24 10:17:44 -0300

    Bump Firebase iOS SDK to 12.15.0

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     name: "PackageName",
     dependencies: [
         .package(url: "https://github.com/onevcat/Kingfisher", from: "8.8.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.10.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "12.15.0"),
         .package(url: "https://github.com/uber/ios-snapshot-test-case", from: "8.0.0")
     ]
 )
